ca1195adb71586332052bc7512a6a92ef7519b4c
Vivek Kushwaha <vivekkushwaha39@gmail.com>
2020-12-19 21:26:44 +0530

    added some more changes for common makefile

---
+++ common.mk
@@ -0,0 +1,25 @@
+SRCS := $(shell find src/ -name "*.cpp" )
+OBJS := $(patsubst %.cpp, objs/%.o, $(SRCS) )
+OBJDIRS := $(shell echo $(OBJS)|xargs dirname |sort|uniq )
+
+INC_DIR=-I./include
+
+all: builddir $(TARGET)
+	@echo "-----------------------------------"
+	@echo "Building $(TARGET)"
+	@echo "-----------------------------------"
+
+builddir: 
+	@echo "----------------------------------"
+	@echo "Creating directories in $(CURDIR)"
+	$(shell echo $(OBJDIRS) |xargs mkdir -p )
+
+	
+objs/%.o : %.cpp
+	echo "compiling $^"
+	g++ -c $< -o $@ $(INC_DIR)
+
+clean:
+	echo "cleaning $(CURDIR)"
+	$(shell rm -rf $(OBJS) $(TARGET) )
+	
